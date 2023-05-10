5d5e4682d8d2fd1023e24b8e8e8bbcee37609cc7
Eric Guyader-Berger <ericguyader@yahoo.fr>
2023-05-10 14:23:13 +0200

    Lesson 6 - step 1 - adds programmatic redirection

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,8 +1,18 @@
 <script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const register = function(){
+    // some registration logic
+    //redirection
+    router.push({name: 'event-details'});
+}
 </script>
 
 <template>
     <p>Register to the event</p>
+    <button @click.prevent="register">Register Me!</button>
 </template>
 
 <style scoped>
