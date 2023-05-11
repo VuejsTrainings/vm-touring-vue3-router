ba7cd2a79f403b9616cf0c19c41065d285c8447a
Eric Guyader-Berger <ericguyader@yahoo.fr>
2023-05-11 10:30:26 +0200

    Lesson 9 - step 1 - adds component nav guard before leaving

--- src/views/event/EditView.vue
+++ src/views/event/EditView.vue
@@ -1,4 +1,18 @@
 <script setup>
+import { ref } from 'vue';
+import { onBeforeRouteLeave } from 'vue-router';
+
+const unsavedChanges = ref(true)
+
+onBeforeRouteLeave( () => {
+    if (unsavedChanges.value) {
+        const accept = window.confirm("There are unsaved changes, are you sure you want to leave?")
+        if(! accept) {
+            return false
+        }
+    }
+});
+
 </script>
 
 <template>
